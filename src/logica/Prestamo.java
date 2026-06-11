078afc8a0e4e792bb5dab1bf8afa63a9a9392a7a
Diego <pito.rodriguez05@gmail.com>
2026-06-10 21:29:27 -0600

    prestamos

--- src/logica/Prestamo.java
+++ src/logica/Prestamo.java
@@ -1,5 +1,31 @@
 package logica;
 
+import java.util.ArrayList;
+
 public class Prestamo {
 
-}
+    private Persona prestatario;
+    private ArrayList<Item> itemsPrestados;
+    private Alerta alertaAsociada;
+    private String fechaPrestamo;
+
+    public Prestamo(Persona prestatario, ArrayList<Item> items, Alerta alertaAsociada) {
+        this.prestatario = prestatario;
+        this.itemsPrestados = new ArrayList<Item>();
+        this.alertaAsociada = alertaAsociada;
+        this.fechaPrestamo = "";
+
+        if (items != null) {
+            for (Item item : items) {
+                agregarItem(item);
+            }
+        }
+
+        if (prestatario != null) {
+            prestatario.agregarPrestamo(this);
+        }
+    }
+
+    public Persona getPrestatario() {
+        return prestatario;
+    }
