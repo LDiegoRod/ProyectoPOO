7a233582fbe0962ce901a93f5cf8f8790bf088fb
Diego <pito.rodriguez05@gmail.com>
2026-06-10 21:12:18 -0600

    alerta1

--- src/logica/Alerta.java
+++ src/logica/Alerta.java
@@ -2,4 +2,12 @@ package logica;
 
 public class Alerta {
 
-}
+    private String mensaje;
+    private boolean esRecurrente;
+    private int tiempoActivacion;
+
+    public Alerta(String mensaje, boolean esRecurrente, int tiempoActivacion) {
+        this.mensaje = mensaje;
+        this.esRecurrente = esRecurrente;
+        this.tiempoActivacion = tiempoActivacion;
+    }
