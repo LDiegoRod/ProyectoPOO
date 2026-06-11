c16917bf39ff4248b8e85a93bbb129e64e8ff414
Diego <pito.rodriguez05@gmail.com>
2026-06-10 20:43:53 -0600

    parte persona

--- src/logica/Persona.java
+++ src/logica/Persona.java
@@ -1,5 +1,15 @@
-package logica;
+import java.util.ArrayList;
 
 public class Persona {
 
-}
+    private String nombre;
+    private String telefono;
+    private String correoElectronico;
+    private ArrayList<Prestamo> prestamos;
+
+    public Persona(String nombre, String telefono, String correoElectronico) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correoElectronico = correoElectronico;
+        this.prestamos = new ArrayList<Prestamo>();
+    }
