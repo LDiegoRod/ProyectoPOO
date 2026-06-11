6e646c9e6beb9cb536a0445ab13147ce54e7cc1d
Diego <pito.rodriguez05@gmail.com>
2026-06-10 21:35:29 -0600

    prestamo controlador

--- src/control/PrestamoControlador.java
+++ src/control/PrestamoControlador.java
@@ -1,5 +1,26 @@
 package control;
 
+import java.util.ArrayList;
+
+import logica.Alerta;
+import logica.Categoria;
+import logica.Item;
+import logica.Persona;
+import logica.Prestamo;
+import logica.Tipo;
+
 public class PrestamoControlador {
 
-}
+    private ArrayList<Item> listaItems;
+    private ArrayList<Persona> listaPersonas;
+    private ArrayList<Categoria> listaCategorias;
+    private ArrayList<Tipo> listaTipos;
+    private ArrayList<Prestamo> listaPrestamos;
+
+    public PrestamoControlador() {
+        this.listaItems = new ArrayList<Item>();
+        this.listaPersonas = new ArrayList<Persona>();
+        this.listaCategorias = new ArrayList<Categoria>();
+        this.listaTipos = new ArrayList<Tipo>();
+        this.listaPrestamos = new ArrayList<Prestamo>();
+    }
