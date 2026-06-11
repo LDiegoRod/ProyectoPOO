3d252abf95d730f3846210a3742fd3ffa0f541db
Diego <pito.rodriguez05@gmail.com>
2026-06-10 21:08:55 -0600

    persona

--- src/logica/Persona.java
+++ src/logica/Persona.java
@@ -1,3 +1,5 @@
+package logica;
+
 import java.util.ArrayList;
 
 public class Persona {
@@ -13,3 +15,49 @@ public class Persona {
         this.correoElectronico = correoElectronico;
         this.prestamos = new ArrayList<Prestamo>();
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public ArrayList<Prestamo> getPrestamos() {
+        return prestamos;
+    }
+
+    public void agregarPrestamo(Prestamo prestamo) {
+        if (prestamo != null && !prestamos.contains(prestamo)) {
+            prestamos.add(prestamo);
+        }
+    }
+
+    public void borrarPrestamo(Prestamo prestamo) {
+        if (prestamo != null) {
+            prestamos.remove(prestamo);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " - " + telefono;
+    }
+}
