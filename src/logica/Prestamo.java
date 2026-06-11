618d94f5516c0f92251b3219a2911efa09bc3969
Diego <pito.rodriguez05@gmail.com>
2026-06-10 21:30:05 -0600

    prestamos 1

--- src/logica/Prestamo.java
+++ src/logica/Prestamo.java
@@ -29,3 +29,53 @@ public class Prestamo {
     public Persona getPrestatario() {
         return prestatario;
     }
+
+    public void setPrestatario(Persona prestatario) {
+        if (this.prestatario != null) {
+            this.prestatario.borrarPrestamo(this);
+        }
+
+        this.prestatario = prestatario;
+
+        if (prestatario != null) {
+            prestatario.agregarPrestamo(this);
+        }
+    }
+
+    public ArrayList<Item> getItemsPrestados() {
+        return itemsPrestados;
+    }
+
+    public void agregarItem(Item item) {
+        if (item != null && !itemsPrestados.contains(item)) {
+            itemsPrestados.add(item);
+        }
+    }
+
+    public void eliminarItem(Item item) {
+        if (item != null) {
+            itemsPrestados.remove(item);
+        }
+    }
+
+    public Alerta getAlertaAsociada() {
+        return alertaAsociada;
+    }
+
+    public void setAlertaAsociada(Alerta alertaAsociada) {
+        this.alertaAsociada = alertaAsociada;
+    }
+
+    public String getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(String fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    @Override
+    public String toString() {
+        return "Prestamo de " + prestatario + " - Items: " + itemsPrestados.size();
+    }
+}
