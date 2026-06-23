ad7adee369e41700bf4b832d2701cae1b03c63c6
Diego <pito.rodriguez05@gmail.com>
2026-06-22 22:27:30 -0600

    diagrama y correcion

--- src/logica/Alerta.java
+++ src/logica/Alerta.java
@@ -5,11 +5,13 @@ public class Alerta {
     private String mensaje;
     private boolean esRecurrente;
     private int tiempoActivacion;
+    private String fechaInicio;
 
-    public Alerta(String mensaje, boolean esRecurrente, int tiempoActivacion) {
+    public Alerta(String mensaje, boolean esRecurrente, int tiempoActivacion, String fechaInicio) {
         this.mensaje = mensaje;
         this.esRecurrente = esRecurrente;
         this.tiempoActivacion = tiempoActivacion;
+        this.fechaInicio = fechaInicio;
     }
 
     public String getMensaje() {
@@ -36,9 +38,17 @@ public class Alerta {
         this.tiempoActivacion = tiempoActivacion;
     }
 
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
     @Override
     public String toString() {
         String tipo = esRecurrente ? "Recurrente" : "Una vez";
-        return mensaje + " (" + tipo + ", tiempo: " + tiempoActivacion + ")";
+        return mensaje + " (" + tipo + ", tiempo: " + tiempoActivacion + ", inicio: " + fechaInicio + ")";
     }
 }
